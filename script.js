e09b438520c54597c1ac7f242809ce34d1d385de
byron-a <byronayalogu@gmail.com>
2022-11-01 20:48:33 +0100

    Removed the hard reload and menu bar

--- script.js
+++ script.js
@@ -1,47 +1,47 @@
 let nav = document.getElementsByClassName('n');
 
 function navbarContact(){
-    setInterval(()=>{
+    setTimeout(()=>{
     nav[5].classList.add('appearAgain')
     skill()
 },100) 
 }
 function skill(){
-    setInterval(()=>{
+    setTimeout(()=>{
     nav[4].classList.add('appearAgain')
     work()
 },300) 
 }
 function work(){
-    setInterval(()=>{
+    setTimeout(()=>{
     nav[3].classList.add('appearAgain')
     hobby()
 },300) 
 }
 function hobby(){
-    setInterval(()=>{
+    setTimeout(()=>{
     nav[2].classList.add('appearAgain')
     education()
 },300) 
 }
 function education(){
-    setInterval(()=>{
+    setTimeout(()=>{
     nav[1].classList.add('appearAgain')
     about()
 },300) 
 }
 function about(){
-    setInterval(()=>{
+    setTimeout(()=>{
     nav[0].classList.add('appearAgain')
     
 },300) 
 }
 
-//second menu click
+//second menu click  ( /(?:^|\s)MyClass(?!\S)/g , '' )
 function navbarRemove(){
+
     for(let n of nav){
-        n.classList.add('disappearAgain')
-        location.reload()
+        n.classList.remove('appearAgain')
     }
 }
 
