c42091280c1442b0a1e351263490b5933152f1cd
byron <byronayalogu@gmail.com>
2024-08-08 15:26:06 +0100

    navbar mobile order

--- script.js
+++ script.js
@@ -3,38 +3,38 @@ let nav = document.getElementsByClassName('n');
 function navbarContact(){
     setTimeout(()=>{
     nav[5].classList.add('appearAgain')
-    skill()
+    about()
 },100) 
 }
 function skill(){
     setTimeout(()=>{
     nav[4].classList.add('appearAgain')
-    work()
-},300) 
+    // work()
+},100) 
 }
 function work(){
     setTimeout(()=>{
     nav[3].classList.add('appearAgain')
-    hobby()
-},300) 
+    skill()
+},100) 
 }
 function hobby(){
     setTimeout(()=>{
     nav[2].classList.add('appearAgain')
-    education()
-},300) 
+    work()
+},100) 
 }
 function education(){
     setTimeout(()=>{
     nav[1].classList.add('appearAgain')
-    about()
-},300) 
+    hobby()
+},100) 
 }
 function about(){
     setTimeout(()=>{
     nav[0].classList.add('appearAgain')
-    
-},300) 
+    education()
+},100) 
 }
 
 //second menu click  ( /(?:^|\s)MyClass(?!\S)/g , '' )
